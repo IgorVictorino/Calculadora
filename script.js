4b4e28c1453f74920e5f9037c07febe659896d71
IgorVictorino <victorino.igor13@gmail.com>
2022-12-09 02:39:54 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,28 @@
+function inserir(num) {
+    var numero = document.getElementById('resposta').innerHTML
+    document.getElementById('resposta').innerHTML = numero + num
+}
+
+function limpar() {
+    document.getElementById('resposta').innerHTML = ""
+}
+
+function apagar() {
+    var resultado = document.getElementById('resposta').innerHTML
+    document.getElementById('resposta').innerHTML = resultado.substring(0, resultado.length - 1)
+}
+
+function calculo() {
+    var resultado = document.getElementById('resposta').innerHTML
+    if (resultado) {
+        document.getElementById('resposta').innerHTML = eval(resultado);
+    }
+    else {
+        let resposta = document.getElementById('resposta')  
+        resposta.style.fontSize = "30px" 
+        resposta.style.width = "315px"
+        resposta.style.paddingRight = "45px" 
+        resposta.innerHTML = "nada para calcular"
+
+    }
+}
