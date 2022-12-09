90faa3e955a95461494677adea3780a85793fbf8
Igor <120154683+IgorVictorino@users.noreply.github.com>
2022-12-09 02:53:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ function limpar() {
     document.getElementById('resposta').innerHTML = ""
 }
 
+
 function apagar() {
     var resultado = document.getElementById('resposta').innerHTML
     document.getElementById('resposta').innerHTML = resultado.substring(0, resultado.length - 1)
 }
+
+//Função para calcular imprimir a ressposta;
 
 function calculo() {
     var resultado = document.getElementById('resposta').innerHTML
